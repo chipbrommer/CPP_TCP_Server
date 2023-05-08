aca21cb64b3c0e3a622d7df8667bad41c55a32ae
Chip Brommer <chipbrommer@gmail.com>
2023-05-07 21:26:33 -0500

    complete basic echo functionality for windows. Still working on linux.

--- CPP_TCP_Server/main.cpp
+++ CPP_TCP_Server/main.cpp
@@ -1,8 +1,32 @@
 ﻿
 #include <iostream>
+#include "Source/tcp_server.h"
 
 int main()
 {
 	std::cout << "Hello CMake." << std::endl;
-	return 0;
+	
+    std::string ipAddress = "127.0.0.1";  // Example IP address
+    int16_t port = 8080;  // Example port
+
+    Essentials::Communications::TCP_Server server(ipAddress, port);
+    std::cout << Essentials::Communications::TcpServerVersion;
+
+    if (server.Start() < 0) 
+    {
+        std::cerr << "Failed to start the server." << std::endl;
+        std::cout << server.GetLastError();
+        return 1;
+    }
+
+    for (;;)
+    {
+        std::cout << ".";
+    }
+
+    // Server is running...
+
+    server.Stop();
+
+    return 0;
 }
